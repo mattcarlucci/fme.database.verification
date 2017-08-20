85910af9aa2f4ede9b751da433b7ce03707519e2
Matt Carlucci <m.carlucci@fme-us.com>
2017-08-20 11:53:40 -0400

    Version 2.0.0.0

--- Fme.DqlProvider/DqlCredentials.cs
+++ Fme.DqlProvider/DqlCredentials.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿// ***********************************************************************
+// Assembly         : Fme.DqlProvider
+// Author           : mcarlucci
+// Created          : 08-20-2017
+//
+// Last Modified By : mcarlucci
+// Last Modified On : 08-20-2017
+// ***********************************************************************
+// <copyright file="DqlCredentials.cs" company="">
+//     Copyright ©  2017
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security;
@@ -7,11 +20,25 @@ using System.Threading.Tasks;
 
 namespace Fme.DqlProvider
 {
+    /// <summary>
+    /// Class DqlCredentials. This class cannot be inherited.
+    /// </summary>
     public sealed class DqlCredentials
     {
+        /// <summary>
+        /// The password
+        /// </summary>
         private readonly SecureString password;
+        /// <summary>
+        /// The user identifier
+        /// </summary>
         private readonly string userId;
-        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DqlCredentials"/> class.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <param name="password">The password.</param>
         public DqlCredentials(string userId, SecureString password)
         {
             this.password = password;
